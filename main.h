3a8ffe63ae6f3e0f38182b97ef2262c27568fd84
antoine lafitte <antoine.lafitte99411@gmail.com>
2024-07-22 15:35:37 +0200

    main.h done

--- main.h
+++ main.h
@@ -2,9 +2,13 @@
 #define main.h
 
 #include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h> /** For the _putchar to use write */
 
-int _putchar(char c)
-int print_char(va_list c);
-int print_string(va_list s);
+int _putchar(char c);
+int print_char(va_list args);
+int print_string(va_list args);
+int print_percent(va_list args);
+int _printf(const char *format, ...);
 
 #endif
