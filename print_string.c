010c29e8c916a9f842e3b921268c5066371d62b4
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-22 16:04:39 +0200

    Print_string done

--- print_string.c
+++ print_string.c
@@ -1,8 +1,30 @@
 #include "main.h"
 
 /**
- *
- *
- *
+ * print_string - the function that print a string
+ * @args: the list of argument
+ * Return: the length of the string
  */
+
 int print_string(va_list args)
+{
+	int i = 0;
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+		_putchar('(');
+		_putchar('n');
+		_putchar('u');
+		_putchar('l');
+		_putchar('l');
+		_putchar(')');
+	}
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	return (i);
+}
