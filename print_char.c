3ffc1cf4d5b5c9aa92ace1ea243fef1ff04319f3
Thomas Maye <thomas.maye.33@gmail.com>
2024-07-23 10:40:08 +0200

    Update the files print_char.c

--- print_char.c
+++ print_char.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
 /**
- * print_char - Prints a character.
- * @args: Arguments list.
- *
+ * print_char - Prints a character
+ * @args: Arguments list
  * Return: Number of characters printed
  */
+
 int print_char(va_list args)
 {
 	char c = va_arg(args, int);
+
 	_putchar(c);
 	return (1);
 }
